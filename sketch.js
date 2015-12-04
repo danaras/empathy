3a91e491fdbb038b725e4af7cdb86f98d29c6005
Danara Sarioglu <ds4489@nyu.edu>
2015-12-03 22:11:55 -0500

    emotions working

--- sketch.js
+++ sketch.js
@@ -4,7 +4,10 @@ var movers=[];
 var tracker;
 var facePos;
 var users=[];
-
+var emotionList=[0,0,0,0];
+var limited;
+var ec;
+var er;
 	// var socket = io.connect('http://45.55.48.195:8080/');
 
 	// socket.on('connect', function() {
@@ -24,23 +27,18 @@ function setup() {
   	//createCanvas(windowWidth, windowHeight);
   	background(255);
 
-  	capture = createCapture({
-  		video: {
-  			mandatory: {
-  				minWidth: 320,
-  				minHeight: 180
-  			}
-  		}
-  	});
-  	createCanvas(320, 180);
-  	//console.log("sia"+capture.size)
-  	capture.size(320, 180);
+  	 createCanvas(320, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
   	capture.id('myFace');
   	console.log(select('#myFace'));
   	capture.hide();
 	tracker = new clm.tracker();
 	tracker.init(pModel);
 	tracker.start(capture.elt);
+	ec = new emotionClassifier();
+	ec.init(emotionModel);
+	//var emotionData = ec.getBlank();
   	for (var i=0; i<50; i++) {
   		movers.push(new Brush());
   	}
@@ -50,7 +48,37 @@ function setup() {
 background(255,20);
   	capture.loadPixels();
   	if(capture.pixels!=0){
-facePos = tracker.getCurrentPosition();
+facePos = tracker.getCurrentParameters();
+if(facePos){
+
+er = ec.meanPredict(facePos);
+					if (er) {
+					
+						for (var i = 0;i < er.length;i++) {
+					
+							if (er[3].value > 0.8) {
+								emotionList[3]=1;
+							} else {
+								emotionList[3]=0;
+							}
+							if (er[0].value > 0.3) {
+								emotionList[0]=1;
+							} else {
+								emotionList[0]=0;
+							}
+							if (er[1].value > 0.4) {
+								emotionList[1]=1;
+							} else {
+								emotionList[1]=0;
+							}
+							if (er[2].value > 0.9) {
+								emotionList[2]=1;
+							} else {
+								emotionList[2]=0;
+							}
+						}
+					}
+				}
 //console.log("middle of your face x:"+ facePos[62][0]+", y:"+facePos[62][1])
   //console.log(capture.pixels.length);
   //loadPixels();
@@ -73,7 +101,6 @@ movers[i].displays();
 }
 }
 }
-
 var Brush = function(){
 	this.posX=0;
 	this.posY=0;
@@ -101,12 +128,39 @@ Brush.prototype.update=function(){
 	this.acceleration=this.direction.normalize();
 	
 }else{*/
+	
+//if no emotions then normalize the acceleration if happy then add acceleration without normalizing
+//console.log(emotionList);
+	if (emotionList[0]==1) {
+		this.acceleration.x=int(randomGaussian()*10);
+	this.acceleration.y=int(randomGaussian()*10);
+		limited =7;
+		console.log("angry");
+	} else if(emotionList[1] ==1) {
+		this.acceleration.x=this.posX;
+	this.acceleration.y=this.posY;
+		limited =1;
+console.log("sad");
+	}else if(emotionList[2] ==1) {
+	this.acceleration.x=int(randomGaussian()*5);
+	this.acceleration.y=int(randomGaussian()*5);
+		limited =5;
+console.log("surprised");
+	} else if(emotionList[3] ==1) {
+		this.acceleration.x=this.posX;
+	this.acceleration.y=this.posY;
+		limited =7;
+console.log("happy");
+}else{
+	console.log("no emotions detected");
 	this.acceleration.x=this.posX;
 	this.acceleration.y=this.posY;
-//if no emotions then normalize the acceleration if happy then add acceleration without normalizing
+	limited =4;
+}
+
 	this.velocity.add(this.acceleration);
 	this.loc.add(this.velocity);
-	this.velocity.limit(5);
+	this.velocity.limit(limited);
 }
 
 Brush.prototype.boundaries=function(){
@@ -129,7 +183,7 @@ Brush.prototype.displays=function(){
 
 	//socket.emit('brush', {oldPosXU:this.oldPosX, oldPosYU:this.oldPosY, locXU:this.loc.x, locYU})
 //this.pix=int(this.loc.x)+int(this.loc.y)*int(width);
-this.pix = int(this.loc.y) * int(capture.width)*4 + int(this.loc.x)*4;
+this.pix = int(this.loc.y) * int(width)*4 + int(this.loc.x)*4;
 // console.log("loc"+this.pix);
 // console.log("R"+capture.pixels[this.pix+1]);
 // console.log("g"+capture.pixels[this.pix+2]);
@@ -139,8 +193,8 @@ this.g=capture.pixels[int(this.pix+1)];
 this.b=capture.pixels[int(this.pix+2)];
 //this.g=green(capture.pixels[int(this.pix)]);
 // this.b=blue(capture.pixels[int(this.pix)]);
-stroke(this.r,this.g,this.b);
-//stroke(0);
+//stroke(this.r,this.g,this.b);
+stroke(0);
 strokeWeight(random(1,4));
 line(this.oldPosX,this.oldPosY,this.loc.x,this.loc.y);
 
